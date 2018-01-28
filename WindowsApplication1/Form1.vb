60c14765d189574518f9bafdc239230a1647b580
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-28 19:09:20 +0800

    更换窗口左上角图标

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -1,7 +1,17 @@
 ﻿Public Class Form1
     Public Sub New()
-        Me.Text = "Center"
+
+        Me.Text = "Icon"
         Me.Size = New Size(250, 200)
+
+        Try
+            Icon = New Icon("res/web.ico")
+        Catch e As Exception
+            Console.WriteLine(e.Message)
+            Environment.Exit(1)
+        End Try
+
         Me.CenterToScreen()
+
     End Sub
 End Class
