36216fb47b23d170c035c108b429b782f3102474
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-28 19:31:29 +0800

    右下角锚定按钮布局

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -1,8 +1,15 @@
 ﻿Public Class Form1
+    Private Const WIDTH1 = 250
+    Private Const HEIGHT1 = 150
+    Private Const BUTTONS_SPACE = 15
+    Private Const PANEL_SPACE = 8
+    Private Const CLOSE_SPACE = 10
+
+
     Public Sub New()
 
-        Me.Text = "Editor"
-        Me.Size = New Size(220, 170)
+        Me.Text = "Buttons"
+        Me.Size = New Size(WIDTH1, HEIGHT1)
 
         Me.InitUI()
 
@@ -12,21 +19,33 @@
 
     Private Sub InitUI()
 
-        Dim mainMenu As New MainMenu
-        Dim file As MenuItem = mainMenu.MenuItems.Add("&File")
-        file.MenuItems.Add(New MenuItem("E&xit",
-                                        New EventHandler(AddressOf Me.OnExit), Shortcut.CtrlX))
+        Dim ok As New Button
 
-        Menu = mainMenu
+        Dim panelHeight As New Integer
+        panelHeight = ok.Height + PANEL_SPACE
 
-        Dim tb As New TextBox
-        tb.Parent = Me
-        tb.Dock = DockStyle.Fill
-        tb.Multiline = True
+        Dim panel As New Panel
+        panel.Height = panelHeight
+        panel.Dock = DockStyle.Bottom
+        panel.Parent = Me
 
-        Dim sb As New StatusBar
-        sb.Parent = Me
-        sb.Text = "Ready"
+        Dim x As Integer = ok.Width * 2 + BUTTONS_SPACE
+        Dim y As Integer = (panelHeight - ok.Height) / 2
+
+        ok.Text = "Ok"
+        ok.Parent = panel
+        ok.Location = New Point(WIDTH1 - x, y)
+        ok.Anchor = AnchorStyles.Right
+
+        Dim close As New Button
+
+        x = close.Width
+
+        close.Text = "Close"
+        close.Parent = panel
+        close.Location = New Point(WIDTH1 - x - CLOSE_SPACE, y)
+        close.Anchor = AnchorStyles.Right
+
 
     End Sub
 
