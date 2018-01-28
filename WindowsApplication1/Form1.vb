a5397b4de0fb48ecfcaa4be79daf198dd4f1f5d7
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-28 19:20:14 +0800

    锚布局测试

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -1,7 +1,32 @@
 ﻿Public Class Form1
     Public Sub New()
-        Me.Text = "Center"
-        Me.Size = New Size(250, 200)
+
+        Me.Text = "Check menu item"
+        Me.Size = New Size(380, 220)
+
+        Me.InitUI()
+
         Me.CenterToScreen()
+
+    End Sub
+
+    Private Sub InitUI()
+
+        Me.Text = "Anchor"
+        Size = New Size(210, 210)
+
+        Dim btn1 As New Button
+        btn1.Text = "Button"
+        btn1.Parent = Me
+        btn1.Location = New Point(30, 30)
+
+        Dim btn2 As New Button
+        btn2.Text = "Button"
+        btn2.Parent = Me
+        btn2.Location = New Point(30, 80)
+        btn2.Anchor = AnchorStyles.Right
+
+        Me.CenterToScreen()
+
     End Sub
 End Class
