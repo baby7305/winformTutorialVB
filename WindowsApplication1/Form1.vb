bd729445265e4ad10a378e26b641fb87b9ac3c19
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-29 16:54:03 +0800

    正方形渐变测试

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -3,8 +3,8 @@
 Public Class Form1
     Public Sub New()
 
-        Me.Text = "Basic Shapes"
-        Me.Size = New Size(420, 280)
+        Me.Text = "Transparent rectangles"
+        Me.Size = New Size(590, 110)
 
         AddHandler Me.Paint, AddressOf Me.OnPaint
 
@@ -16,25 +16,12 @@ Public Class Form1
     Private Sub OnPaint(ByVal sender As Object, ByVal e As PaintEventArgs)
 
         Dim g As Graphics = e.Graphics
-        g.SmoothingMode = SmoothingMode.AntiAlias
 
-        g.FillRectangle(Brushes.Gray, 20, 20, 120, 80)
-        g.FillRectangle(Brushes.Gray, 180, 20, 80, 80)
-
-        g.FillEllipse(Brushes.Gray, 30, 120, 100, 100)
-        g.FillEllipse(Brushes.Gray, 160, 130, 100, 70)
-
-        Dim points(5) As Point
-
-        points(0) = New Point(300, 40)
-        points(1) = New Point(340, 15)
-        points(2) = New Point(380, 40)
-        points(3) = New Point(380, 80)
-        points(4) = New Point(340, 105)
-        points(5) = New Point(300, 80)
-
-        g.FillPolygon(Brushes.Gray, points)
-        g.FillPie(Brushes.Gray, New Rectangle(290, 130, 90, 90), 0, 315)
+        For i As Integer = 1 To 10
+            Dim color As Color = Color.FromArgb(i * 25, 0, 0, 255)
+            Dim brush As Brush = New SolidBrush(color)
+            g.FillRectangle(brush, 50 * i, 20, 40, 40)
+        Next
 
         g.Dispose()
 
