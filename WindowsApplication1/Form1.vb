48aeafdf3c1397d437478633be5c0a6b6e826405
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-29 14:24:25 +0800

    颜色选择器对话框测试

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -1,9 +1,14 @@
 ﻿Public Class Form1
-    Dim statusbar As StatusBar
+    Private col As Color
+
+    Private Const rectWidth As Integer = 100
+    Private Const rectHeight As Integer = 100
+    Private r As Rectangle
+
 
     Public Sub New()
 
-        Me.Text = "FolderBrowserDialog"
+        Me.Text = "ColorDialog"
         Me.Size = New Size(300, 250)
 
         Me.InitUI()
@@ -14,29 +19,50 @@
 
     Private Sub InitUI()
 
-        Dim toolbar As New ToolBar
+        Dim tbar As New ToolBar
         Dim open As New ToolBarButton
-        open.Text = "file"
+        open.Text = "color"
 
-        statusbar = New StatusBar
-        statusbar.Parent = Me
+        col = Color.Blue
 
-        toolbar.Buttons.Add(open)
+        tbar.Buttons.Add(open)
 
-        Me.Controls.Add(toolbar)
+        Me.LocateRect()
 
-        AddHandler toolbar.ButtonClick, AddressOf Me.OnClicked
+        Me.SetStyle(ControlStyles.ResizeRedraw, True)
+        Controls.Add(tbar)
+
+        AddHandler Me.Paint, AddressOf Me.OnPaint
+        AddHandler tbar.ButtonClick, AddressOf Me.OnClicked
+
+    End Sub
+
+    Private Sub OnPaint(ByVal sender As Object, ByVal e As PaintEventArgs)
+
+        Dim g As Graphics = e.Graphics
+        Me.LocateRect()
+
+        Dim brsh As New SolidBrush(col)
+
+        g.FillRectangle(brsh, r)
 
     End Sub
 
     Private Sub OnClicked(ByVal sender As Object,
                           ByVal e As ToolBarButtonClickEventArgs)
 
-        Dim dialog As New FolderBrowserDialog
+        Dim dialog As New ColorDialog
 
         If dialog.ShowDialog(Me) = DialogResult.OK Then
-            statusbar.Text = dialog.SelectedPath
+            col = dialog.Color
+            Me.Invalidate()
         End If
 
+    End Sub
+
+    Private Sub LocateRect()
+        Dim x As Integer = (Me.ClientSize.Width - rectWidth) / 2
+        Dim y As Integer = (Me.ClientSize.Height - rectHeight) / 2
+        r = New Rectangle(x, y, rectWidth, rectHeight)
     End Sub
 End Class
