83eaf48024811ade6894dc78a2c1eef3a266c1cc
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-29 14:01:21 +0800

    简单文件菜单测试

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -1,7 +1,33 @@
 ﻿Public Class Form1
     Public Sub New()
-        Me.Text = "Center"
-        Me.Size = New Size(250, 200)
+
+        Me.Text = "Simple menu"
+        Me.Size = New Size(220, 170)
+
+        Me.InitUI()
+
         Me.CenterToScreen()
+
+    End Sub
+
+    Private Sub InitUI()
+
+        Dim ms As New MenuStrip
+        ms.Parent = Me
+
+        Dim fileItem As New ToolStripMenuItem("&File")
+        Dim exitItem As New ToolStripMenuItem("&Exit", Nothing,
+                                              New EventHandler(AddressOf OnExit))
+
+        exitItem.ShortcutKeys = Keys.Control Or Keys.X
+        fileItem.DropDownItems.Add(exitItem)
+
+        ms.Items.Add(fileItem)
+        MainMenuStrip = ms
+
+    End Sub
+
+    Private Sub OnExit(ByVal sender As Object, ByVal e As EventArgs)
+        Me.Close()
     End Sub
 End Class
