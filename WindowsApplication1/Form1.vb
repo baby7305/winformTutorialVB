b677c4b623d3b1c31346177b9e936a7c338b93a1
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-29 14:22:46 +0800

    树形文件浏览对话框测试

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -1,7 +1,42 @@
 ﻿Public Class Form1
+    Dim statusbar As StatusBar
+
     Public Sub New()
-        Me.Text = "Center"
-        Me.Size = New Size(250, 200)
+
+        Me.Text = "FolderBrowserDialog"
+        Me.Size = New Size(300, 250)
+
+        Me.InitUI()
+
         Me.CenterToScreen()
+
+    End Sub
+
+    Private Sub InitUI()
+
+        Dim toolbar As New ToolBar
+        Dim open As New ToolBarButton
+        open.Text = "file"
+
+        statusbar = New StatusBar
+        statusbar.Parent = Me
+
+        toolbar.Buttons.Add(open)
+
+        Me.Controls.Add(toolbar)
+
+        AddHandler toolbar.ButtonClick, AddressOf Me.OnClicked
+
+    End Sub
+
+    Private Sub OnClicked(ByVal sender As Object,
+                          ByVal e As ToolBarButtonClickEventArgs)
+
+        Dim dialog As New FolderBrowserDialog
+
+        If dialog.ShowDialog(Me) = DialogResult.OK Then
+            statusbar.Text = dialog.SelectedPath
+        End If
+
     End Sub
 End Class
