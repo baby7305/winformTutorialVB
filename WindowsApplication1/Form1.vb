222d09ae561c421154f8610316335277643a5524
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-29 16:56:05 +0800

    图像加载变灰测试

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -1,10 +1,16 @@
 ﻿Imports System.Drawing.Drawing2D
 
 Public Class Form1
+    Private rotunda As Bitmap
+    Private gs As Bitmap
+
     Public Sub New()
 
-        Me.Text = "Transparent rectangles"
-        Me.Size = New Size(590, 110)
+        Me.Text = "Grayscale"
+        Me.Size = New Size(290, 150)
+
+        rotunda = Me.LoadImage
+        gs = GrayScale(rotunda.Clone)
 
         AddHandler Me.Paint, AddressOf Me.OnPaint
 
@@ -12,16 +18,45 @@ Public Class Form1
 
     End Sub
 
+    Private Function LoadImage() As Bitmap
+
+        Try
+            rotunda = New Bitmap("res/rotunda.jpg")
+            Return rotunda
+        Catch
+            Console.WriteLine("Image not found")
+            Environment.Exit(1)
+        End Try
+
+    End Function
+
+    Private Function GrayScale(ByVal image As Bitmap) As Bitmap
+
+        Dim w As Integer = image.Width
+        Dim h As Integer = image.Height
+
+        For i As Integer = 0 To w - 1
+            For j As Integer = 0 To h - 1
+                Dim c As Color = image.GetPixel(i, j)
+                Dim lum As Double = 0.299 * c.R + 0.587 * c.G + 0.114 * c.B
+                image.SetPixel(i, j, Color.FromArgb(lum, lum, lum))
+            Next
+        Next
+
+        Return image
+
+    End Function
+
 
     Private Sub OnPaint(ByVal sender As Object, ByVal e As PaintEventArgs)
 
         Dim g As Graphics = e.Graphics
 
-        For i As Integer = 1 To 10
-            Dim color As Color = Color.FromArgb(i * 25, 0, 0, 255)
-            Dim brush As Brush = New SolidBrush(color)
-            g.FillRectangle(brush, 50 * i, 20, 40, 40)
-        Next
+        Dim r1 As New Rectangle(15, 15, rotunda.Width, rotunda.Height)
+        g.DrawImage(rotunda, r1)
+
+        Dim r2 As New Rectangle(150, 15, gs.Width, gs.Height)
+        g.DrawImage(gs, r2)
 
         g.Dispose()
 
