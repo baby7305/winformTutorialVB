15de913bf426829e7cac82cb4f4f70e4b25ab7be
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-29 13:56:46 +0800

    树形结构测试

--- WindowsApplication1/Actress.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Actress
-    Public m_name As String
-    Public m_year As Integer
-
-    Public Sub New(ByVal name As String, ByVal year As Integer)
-        Me.m_name = name
-        Me.m_year = year
-    End Sub
-End Class
--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -1,11 +1,11 @@
 ﻿Public Class Form1
     Private sb As StatusBar
-    Private lv As ListView
+    Private WithEvents tv As TreeView
 
     Public Sub New()
 
-        Me.Text = "ListView"
-        Me.Size = New Size(350, 300)
+        Me.Text = "TreeView"
+        Me.Size = New Size(250, 250)
 
         Me.InitUI()
 
@@ -15,68 +15,35 @@
 
     Private Sub InitUI()
 
-        Dim actresses As New List(Of Actress)
+        tv = New TreeView
 
-        actresses.Add(New Actress("Jessica Alba", 1981))
-        actresses.Add(New Actress("Angelina Jolie", 1975))
-        actresses.Add(New Actress("Natalie Portman", 1981))
-        actresses.Add(New Actress("Rachel Weiss", 1971))
-        actresses.Add(New Actress("Scarlett Johansson", 1984))
+        Dim root As New TreeNode
+        root.Text = "Languages"
 
+        Dim child1 As New TreeNode
+        child1.Text = "Python"
 
-        Dim name As New ColumnHeader
-        name.Text = "Name"
-        name.Width = -1
-        Dim year As New ColumnHeader
-        year.Text = "Year"
+        Dim child2 As New TreeNode
+        child2.Text = "Ruby"
 
-        Me.SuspendLayout()
+        Dim child3 As New TreeNode
+        child3.Text = "Java"
 
-        lv = New ListView
-        lv.Parent = Me
-        lv.FullRowSelect = True
-        lv.GridLines = True
-        lv.AllowColumnReorder = True
-        lv.Sorting = SortOrder.Ascending
-        lv.Columns.AddRange(New ColumnHeader() {name, year})
+        root.Nodes.AddRange(New TreeNode() {child1, child2, child3})
 
-        For Each act As Actress In actresses
-            Dim item As New ListViewItem
-            item.Text = act.m_name
-            item.SubItems.Add(act.m_year.ToString())
-            lv.Items.Add(item)
-        Next
-
-        lv.Dock = DockStyle.Fill
+        tv.Parent = Me
+        tv.Nodes.Add(root)
+        tv.Dock = DockStyle.Fill
 
         sb = New StatusBar
         sb.Parent = Me
-        lv.View = View.Details
 
-        Me.ResumeLayout()
-
-        AddHandler lv.Click, AddressOf Me.OnChanged
-        AddHandler lv.ColumnClick, AddressOf Me.OnColumnClick
 
     End Sub
 
-    Private Sub OnChanged(ByVal sender As Object, ByVal e As EventArgs)
-
-        Dim name As String = lv.SelectedItems(0).SubItems(0).Text
-        Dim born As String = lv.SelectedItems(0).SubItems(1).Text
-        sb.Text = name & ", " & born
-
-    End Sub
-
-    Private Sub OnColumnClick(ByVal sender As Object,
-                              ByVal e As ColumnClickEventArgs)
-
-        If sender.Sorting = SortOrder.Ascending Then
-            sender.Sorting = SortOrder.Descending
-        Else
-            sender.Sorting = SortOrder.Ascending
-        End If
-
+    Private Sub OnSelected(ByVal sender As Object,
+                           ByVal e As TreeViewEventArgs) Handles tv.AfterSelect
+        sb.Text = e.Node.Text
     End Sub
 
 End Class
